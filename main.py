65b878561ef2500a4e4be63b542e28dd88bb1c55
MariaMelnikova8 <mariia16melnikova@yandex.ru>
2022-12-12 18:21:52 +0300

    Добавила файл mail.json

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import os
 from dotenv import load_dotenv
 import mail
@@ -7,7 +8,14 @@ load_dotenv()
 login = os.getenv("LOGIN")
 password = os.getenv("PASSWORD")
 
+with open('mail.json', 'r') as file:
+    emails = file.read()
+emails = json.loads(emails)
+
 heading = 'invitaion'
 
 text = ""
+
+for email in emails:
+    mail.send_mail(login, password, heading, text, email)
 
