292b6d9281409c95a930018d6ca5f60b042dbb28
MariaMelnikova8 <mariia16melnikova@yandex.ru>
2023-02-09 18:25:34 +0300

    Создали проект отправки писем

--- mail.py
+++ mail.py
@@ -1,15 +1,18 @@
 import smtplib
 
-
+# Функция формирует и отправляет письмо на заданный почтовый ящик
 def send_mail(login, password, heading, text, mail):
+    # Заголовок для корректной отправки письма
     title = f'''From: {login}
 To: {mail}
 Subject: {heading}
 Content-Type: text/plain; charset="UTF-8";'''
 
+    # Формирование письма для отправки
     letter = f'{title}\n\n{text}'
     letter = letter.encode("UTF-8")
 
+    # Вызов функций инициализации, авторизации пользователя и отправки письма через smtp
     server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
     server.login(login, password)
     server.sendmail(login, mail, letter)
--- main.py
+++ main.py
@@ -2,20 +2,41 @@ import json
 import os
 from dotenv import load_dotenv
 import mail
+import argparse
 
-
+# Добавление в код секретных данных из файла .env
 load_dotenv()
 login = os.getenv("LOGIN")
 password = os.getenv("PASSWORD")
 
-with open('mail.json', 'r') as file:
-    emails = file.read()
-emails = json.loads(emails)
+# Добавление к программе аргументов командной строки
+parser = argparse.ArgumentParser(
+        description='Описание программы'
+)
+parser.add_argument('--m', help='Для файла с почтами.')
+parser.add_argument('--t', help='Для файла с текстом письма.')
+parser.add_argument('--s', help='Для заголовка письма')
+emails_file_path = parser.parse_args().m
+text_file_path = parser.parse_args().t
+theme = parser.parse_args().s
 
-heading = 'invitaion'
+# Открываем файл с почтами
+try:
+    with open(emails_file_path, 'r') as file:
+        emails = file.read()
+    emails = json.loads(emails)
+except FileNotFoundError:
+    print('Файл введен неправильно')
+    exit()
 
-text = ""
+try:
+    with open(text_file_path, 'r') as file:
+        text = file.read()
+except FileNotFoundError:
+    print('Файл введен неправильно')
+    exit()
 
+# Вызов функции в цикле для отправки письма на несколько почт
 for email in emails:
-    mail.send_mail(login, password, heading, text, email)
+    mail.send_mail(login, password, theme, text, email)
 
